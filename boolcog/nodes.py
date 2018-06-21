7ce0f9343e3d6e69d3a4029b04016b5f37a60432
Iwo Herka <hi@iwoherka.eu>
2018-06-21 21:38:09 +0200

    Add non-terminal node compilers

---
+++ boolcog/nodes.py
@@ -0,0 +1,48 @@
+import ast
+
+
+def compile_comparison(first, second, opnode):
+    return ast.Compare(
+        ops = [opnode()],
+        left = first,
+        comparators = [second]
+    )
+
+def compile_eq(first, second):
+    return compile_comparison(first, second, ast.Eq)
+
+def compile_neq(first, second):
+    return compile_comparison(first, second, ast.NotEq)
+
+def compile_lt(first, second):
+    return compile_comparison(first, second, ast.Lt)
+
+def compile_lte(first, second):
+    return compile_comparison(first, second, ast.LtE)
+
+def compile_gt(first, second):
+    return compile_comparison(first, second, ast.Gt)
+
+def compile_gte(first, second):
+    return compile_comparison(first, second, ast.GtE)
+
+def compile_in(first, second):
+    return compile_comparison(first, second, ast.In)
+
+def compile_identifier(name):
+    return ast.Name(id = name, ctx = ast.Load())
+
+def compile_tautology():
+    return ast.NameConstant(value = True)
+
+def compile_contradiction():
+    return ast.NameConstant(value = False)
+
+def compile_not(operand):
+    return ast.UnaryOp(op = ast.Not(), operand = operand)
+
+def compile_and(*values):
+    return ast.BoolOp(op = ast.And(), values = list(values))
+
+def compile_or(*values):
+    return ast.BoolOp(op = ast.Or(), values = list(values))
