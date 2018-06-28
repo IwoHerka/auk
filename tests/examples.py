e552fd66b3ef3cee486f4b8634a2d3f2be58cc5b
Iwo Herka <hi@iwoherka.eu>
2018-06-28 20:19:33 +0200

    Add more examples

--- tests/examples.py
+++ tests/examples.py
@@ -32,6 +32,7 @@ def test_example_2():
     func = compile_predicate(sexp)
     assert func(P, Q) == (not P and not Q)
 
+
 def test_example_3():
     # num in [1, 2, 3] and num >= 2
 
@@ -46,3 +47,48 @@ def test_example_3():
     assert func(2)
     assert func(3)
 
+
+def test_example_4():
+    func = compile_predicate(object())
+    assert func()
+
+
+def test_example_5():
+    sexp = ['neq', object(), object()]
+    func = compile_predicate(sexp)
+    assert func()
+
+
+def test_example_6():
+    genlist = lambda: [x for x in range(7)]
+
+    sexp = \
+        ['lt',
+            ['callable', genlist],
+            [1, 2, 3, 4, 5, 6]
+        ]
+
+    func = compile_predicate(sexp)
+    assert func()
+
+
+def test_example_7():
+    sexp = \
+        ['and',
+            ['eq',
+                ['identifier', 'lives_in'],
+                'London'
+            ],
+            ['eq',
+                ['identifier', 'name'],
+                'John'
+            ],
+        ]
+
+    john = {'name': 'John', 'lives_in': 'London'}
+    mark = {'name': 'Mark', 'lives_in': 'London'}
+
+    func = compile_predicate(sexp)
+
+    assert func(**john)
+    assert not func(**mark)
