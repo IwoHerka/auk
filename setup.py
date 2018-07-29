bf32087ce3cf4db175aa6e0027d7030bd35493b3
Iwo Herka <hi@iwoherka.eu>
2018-07-29 12:48:44 +0000

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name             = 'auk',
-    version          = '0.1.0',
+    version          = '0.1.1',
     license          = 'MIT',
     requires         = ['python (>= 3.4)'],
     provides         = ['auk'],
